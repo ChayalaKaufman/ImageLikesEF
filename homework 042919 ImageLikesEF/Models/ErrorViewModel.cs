e43b67a2f498e2e98b7d4abee5257fc8ebff24d8
Chayala Kaufman <chayalakaufman@gmail.com>
2019-05-04 22:33:42 -0400

    Add project files.

---
+++ homework 042919 ImageLikesEF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace homework_042919_ImageLikesEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
